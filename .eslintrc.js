7369672814e2930a9a9354d20b92cd589061cd27
John Drefahl <john@velobpa.com>
2026-02-15 01:56:50 -0600

    chore: add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
+ * Licensed under the Business Source License 1.1
+ */
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['eslint-plugin-n8n-nodes-base'],
+  extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
+  rules: {
+    'n8n-nodes-base/community-package-json-name-still-default': 'off',
+  },
+};
